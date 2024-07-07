e0da7644c682eb83bbb46aa061ea545b0a1a723f
Matthias Watermann <support@mwat.de>
2024-07-07 22:40:39 +0200

    + new checkin after big refactoring;

---
+++ hashmap.go
@@ -0,0 +1,516 @@
+/*
+Copyright © 2019, 2024  M.Watermann, 10247 Berlin, Germany
+
+		All rights reserved
+	EMail : <support@mwat.de>
+*/
+package hashtags
+
+import (
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"hash/crc32"
+	"io"
+	"os"
+	"slices"
+	"sort"
+	"strings"
+
+	se "github.com/mwat56/sourceerror"
+)
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+type (
+	// `tHashMap` is indexed by #hashtags/@mentions pointing to a `tSourceList`.
+	tHashMap map[string]*tSourceList
+)
+
+var (
+	// `UseBinaryStorage` determines whether to use binary storage
+	// or not (i.e. plain text).
+	//
+	// Loading/storing binary data is about three times as fast with
+	// the `THashList` data than reading and parsing plain text data.
+	UseBinaryStorage = true
+)
+
+// --------------------------------------------------------------------------
+// constructor function
+
+func NewHashMap() tHashMap {
+	hm := make(tHashMap, 64)
+
+	return hm
+} // NewHashMap()
+
+// --------------------------------------------------------------------------
+// helper for sorting the hash strings; used by `kys()` and `sort()`
+
+// `cmp4sort()` helps to sort a slice in ascending order based on the
+// comparison of the substrings after the leading hash or mention mark.
+//
+// It is used in the `sort()` method to ensure that the hash map is sorted,
+// which can improve the performance of certain operations on the hash map,
+// such as searching for a specific key.
+//
+// The function takes two strings as input and returns an integer indicating
+// their relative order. If `a` is less than `b`, it returns `-1`. If a is
+// greater than `b`, it returns `1`. If `a` and `b` are equal, it returns `0`.
+//
+// The function first checks if the first character of `a` and `b` is a hash
+// or mention mark. If so, it removes the leading character from both strings.
+//
+// Returns;
+// - `int`: The result of the comparison the two strings as described above.
+func cmp4sort(a, b string) int {
+	switch a[0] {
+	case MarkHash, MarkMention:
+		a = a[1:]
+	}
+	switch b[0] {
+	case MarkHash, MarkMention:
+		b = b[1:]
+	}
+	if a < b {
+		return -1
+	}
+	if a > b {
+		return 1
+	}
+	return 0
+} // cmp4sort()
+
+// -------------------------------------------------------------------------
+// methods of tHashMap
+
+// `add()` appends `aID` to the sources list associated with `aName`.
+//
+// Parameters:
+// - `aName` is the list index to lookup.
+// - `aID` is to be added to the hash list.
+//
+// Returns:
+// - `*tHashMap`: This hash map.
+func (hm *tHashMap) add(aName string, aID uint64) *tHashMap {
+	aName = strings.ToLower(aName)
+	if sl, exists := (*hm)[aName]; exists {
+		sl.insert(aID) // changes in place
+	} else {
+		(*hm)[aName] = newSourceList().insert(aID)
+	}
+
+	return hm
+} // add()
+
+// `checksum()` computes the list's CRC32 checksum.
+//
+// Returns:
+// - `uint32`: The computed checksum.
+func (hm *tHashMap) checksum() uint32 {
+	// the mutex.Lock is done by the callers
+
+	return crc32.Update(0,
+		crc32.MakeTable(0), // other table types may not be reproducible
+		[]byte(hm.String()))
+} // checksum()
+
+// `clear()` empties the internal data structures:
+// all `#hashtags` and `@mentions` are deleted.
+//
+// Returns:
+// - `*tHashMap`: The cleared hash map.
+func (hm *tHashMap) clear() *tHashMap {
+	// the mutex.Lock is done by the callers
+
+	for hash, sl := range *hm {
+		sl.clear()
+		delete(*hm, hash)
+	}
+
+	return hm
+} // clear()
+
+// `compareTo()` returns whether the current hash map is equal to the
+// provided hash map.
+//
+// Parameters:
+// - `aMap`: The hash map to compare with.
+//
+// Returns:
+// - `bool`: Whether the hash maps are equal.
+func (hm tHashMap) compareTo(aMap tHashMap) bool {
+	if len(hm) != len(aMap) {
+		return false
+	}
+
+	for hash, sl := range hm {
+		osl, ok := aMap[hash]
+		if !ok {
+			return false
+		}
+		if !sl.compareTo(*osl) {
+			return false
+		}
+	}
+
+	return true
+} // compareTo()
+
+// `count()` returns the number of hashtags (if `aDelim == '#'`) or
+// mentions (if `aDelim == '@'`).
+//
+// Parameters:
+// - `aDelim`: The start of words to search (i.e. either '@' or '#').
+//
+// Returns:
+// - `int`: The number of hashtags/mentions.
+func (hm tHashMap) count(aDelim byte) int {
+	var result int
+	for hash := range hm {
+		if hash[0] == aDelim {
+			result++
+		}
+	}
+
+	return result
+} // count()
+
+// `countedList()` returns a list of #hashtags/@mentions with
+// their respective count of associated IDs.
+//
+// Returns:
+// - `TCountList`: A list of #hashtags/@mentions with
+// their respective count of associated IDs.
+func (hm tHashMap) countedList() TCountList {
+	if 0 == len(hm) {
+		return nil
+	}
+
+	// result := make(TCountList, 0, len(hm))
+	result := TCountList{}
+	for name, sl := range hm {
+		result.insert(TCountItem{len(*sl), name})
+	}
+
+	return result // *result.sort()
+} // countedList()
+
+// `idList()` returns a list of #hashtags and @mentions associated with `aID`.
+//
+// Parameters:
+// - `aID`: The referenced object to lookup.
+//
+// Returns:
+// - `[]string`: The list of #hashtags and @mentions associated with `aID`.
+func (hm *tHashMap) idList(aID uint64) []string {
+	// var result []string
+	result := make([]string, 0, len(*hm))
+
+	for hash, sl := range *hm {
+		if 0 > sl.indexOf(aID) {
+			continue // ID not found
+		}
+		result = append(result, hash)
+	}
+
+	if 0 < len(result) {
+		sort.Slice(result, func(i, j int) bool {
+			return (result[i] < result[j]) // ascending
+		})
+	}
+
+	return result
+} // idList()
+
+// `idxLen()` returns the number of IDs stored for `aName`.
+//
+// Parameters:
+// - `aDelim`: The first character of words to use (i.e. either '@' or '#').
+// - `aName`: The hash to lookup.
+//
+// Returns:
+// - `int: The number of references of `aName`, or `-1` if not found.
+func (hm tHashMap) idxLen(aDelim byte, aName string) int {
+	if 0 == len(aName) {
+		return -1
+	}
+
+	aName = strings.ToLower(aName)
+	if aName[0] != aDelim {
+		aName = string(aDelim) + aName
+	}
+
+	if sl, ok := hm[aName]; ok {
+		return len(*sl)
+	}
+
+	return -1
+} // idxLen()
+
+// `keys()` returns a slice of all keys in the hash map.
+// If the hash map is empty, it returns an empty slice.
+//
+// The function does not modify the hash map.
+//
+// Returns:
+// - `[]string`: A sorted slice of all keys in the hash map.
+func (hm tHashMap) keys() []string {
+	hLen := len(hm)
+	if 0 == hLen {
+		var result []string
+		return result
+	}
+
+	// Create a slice to hold the keys
+	keys := make([]string, 0, hLen)
+	// Extract keys from the map
+	for key := range hm {
+		keys = append(keys, key)
+	}
+	// Sort the keys ignoring the respective leading hash/mention mark
+	slices.SortFunc(keys, cmp4sort)
+
+	return keys
+} // keys()
+
+// `list()` returns a list of object IDs associated with `aName`.
+//
+// Parameters:
+// - `aDelim` The start of words to search (i.e. either '@' or '#').
+// - `aName`: The hash to lookup.
+//
+// Returns:
+// - `[]uint64`: The number of references of `aName`.
+func (hm *tHashMap) list(aDelim byte, aName string) (rList []uint64) {
+
+	if 0 == len(aName) {
+		return
+	}
+
+	aName = strings.ToLower(aName)
+	if aName[0] != aDelim {
+		aName = string(aDelim) + aName
+	}
+
+	if sl, ok := (*hm)[aName]; ok {
+		rList = []uint64(*sl)
+	}
+
+	return
+} // list()
+
+// `remove()` deletes `aID` from the list of `aName`.
+//
+// Parameters:
+// - `aDelim` is the start character of words to use (i.e. either '@' or '#').
+// - `aName`: The hash/mention to lookup.
+// - `aID`: The referenced object to remove from the list.
+//
+// Returns:
+// - `*tHashMap`: The current hash map.
+func (hm *tHashMap) remove(aDelim byte, aName string, aID uint64) *tHashMap {
+	aName = strings.ToLower(aName)
+	if (0 == len(aName)) || (0 == aID) {
+		return hm
+	}
+
+	if aName[0] != aDelim {
+		aName = string(aDelim) + aName
+	}
+
+	if sl, ok := (*hm)[aName]; ok {
+		// sl = sl.removeID(aID)
+		sl.removeID(aID)
+		if 0 == len(*sl) {
+			delete(*hm, aName)
+		} else {
+			(*hm)[aName] = sl //TODO: is this needed?
+		}
+	}
+
+	return hm
+} // remove()
+
+// `removeID()` deletes all #hashtags/@mentions associated with `aID`.
+//
+// Parameters:
+// - `aID`: The object to remove from all references list.
+//
+// Returns:
+// - `*tHashMap`: The current hash map.
+func (hm *tHashMap) removeID(aID uint64) *tHashMap {
+	if (nil == hm) || 0 == len(*hm) {
+		return hm
+	}
+	for hash, sl := range *hm {
+		sl = sl.removeID(aID)
+		if 0 == len(*sl) {
+			delete(*hm, hash)
+		} else {
+			(*hm)[hash] = sl
+		}
+	}
+
+	return hm
+} // removeID()
+
+// `renameID()` replaces all occurrences of `aOldID` by `aNewID`.
+//
+// Parameters:
+// - `aOldID`: The ID to be replaced in this list.
+// - `aNewID`: The replacement ID in this list.
+//
+// Returns:
+// - `*tHashMap`: The modified hash map.
+func (hm *tHashMap) renameID(aOldID, aNewID uint64) *tHashMap {
+	if (0 == len(*hm)) || (aOldID == aNewID) {
+		return hm
+	}
+
+	for idx, sl := range *hm {
+		(*hm)[idx] = sl.renameID(aOldID, aNewID)
+	}
+
+	return hm
+} // renameID()
+
+/* */
+
+// `sort()` ensures that the hash map is sorted, which can improve the
+// performance of certain operations on the hash map, such as searching
+// for a specific key.
+// Additionally, sorting the keys can make the hash map easier to read
+// and understand, as it presents the keys in a consistent order.
+//
+// Returns:
+// - `*tHashMap`: The sorted hash map.
+func (hm *tHashMap) sort() *tHashMap {
+	hLen := len(*hm)
+	if 0 == hLen {
+		return hm
+	}
+	/*
+		// Create a slice to hold the keys
+		keys := make([]string, 0, hLen)
+		// Extract keys from the map
+		for key := range *hm {
+			keys = append(keys, key)
+		}
+
+		// Sort the keys ignoring the respective leading hash/mention mark
+		slices.SortFunc(keys, cmp4sort)
+	*/
+
+	keys := hm.keys()
+	// Create a new map to store sorted key-value pairs
+	sortedMap := make(tHashMap, len(keys))
+
+	// Iterate through sorted keys and create a new sorted map
+	for _, key := range keys {
+		sl := (*hm)[key]
+		sortedMap[key] = sl.sort()
+	}
+	(*hm) = sortedMap
+
+	return hm
+} // sort()
+/* */
+
+// `store()` writes the whole hash/mention list to `aFilename`.
+//
+// Parameters:
+// - `aFileName`: Name of the file to use for storing the current hash map.
+//
+// Returns:
+// - `int`: Number of bytes written to storage.
+func (hm tHashMap) store(aFilename string) (int, error) {
+	if "" == aFilename {
+		return 0, se.Wrap(errors.New("missing filename in store()"), 1)
+	}
+
+	file, err := os.OpenFile(aFilename,
+		os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0660) //#nosec G302
+	if nil != err {
+		return 0, se.Wrap(err, 1)
+	}
+	defer file.Close()
+
+	if !UseBinaryStorage {
+		// use plain text storage
+		return file.Write([]byte(hm.String()))
+	}
+
+	encoder := gob.NewEncoder(file)
+	if err = encoder.Encode(hm); nil != err {
+		return 0, se.Wrap(err, 1)
+	}
+	size, err := file.Seek(0, io.SeekEnd)
+	if nil != err {
+		return 0, se.Wrap(err, 2)
+	}
+
+	return int(size), nil
+} // store()
+
+// `String()` is used to generate a footprint of the hash map.
+//
+// It is also used to generate a list of hashtags/@mentions mostly
+// for debugging purposes.
+//
+// NOTE: Since the order of the hashtags/mentions is NOT guaranteed
+// here the order of the the hashtags/mentions im the returned String
+// isn't guaranteed to be ordered either.
+//
+// Returns:
+// - `string`: The string representation of this hash map.
+func (hm *tHashMap) String() (rStr string) {
+	sLen := len(*hm)
+	if 0 == sLen {
+		return
+	}
+
+	keys := hm.keys()
+	// Iterate through sorted keys and create a new sorted string
+	for _, hash := range keys {
+		sl := (*hm)[hash]
+		rStr += fmt.Sprintf("[%s]\n%s", hash, sl.String())
+	}
+	// // NOTE: the order of the items is NOT guaranteed here
+	// for hash, sl := range *hm {
+	// 	rStr += fmt.Sprintf("[%s]\n%s", hash, sl.String())
+	// }
+
+	return
+} // String()
+
+// `walk()` traverses through all entries in the #hashtag/@mention
+// lists calling `aFunc` for each entry.
+//
+// If `aFunc` returns `false` when called the respective ID
+// will be removed from the associated #hashtag/@mention.
+//
+// NOTE: Since the order of the hashtags/mentions is NOT guaranteed
+// here the order of visits to the items isn't ordered either.
+//
+// Parameters:
+// - `aFunc` The function called for each ID in all lists.
+//
+// Returns:
+// - `bool`: `true` if a ID was removed, or `false` otherwise.
+func (hm *tHashMap) walk(aFunc TWalkFunc) bool {
+	var result bool
+	for hash, sl := range *hm {
+		for _, id := range *sl {
+			if !aFunc(hash, id) {
+				hm.removeID(id)
+				result = true
+			}
+		}
+	}
+
+	return result
+} // walk()
+
+/* EoF */
---
+++ hashmap_test.go
@@ -0,0 +1,600 @@
+/*
+Copyright © 2023, 2024  M.Watermann, 10247 Berlin, Germany
+
+		All rights reserved
+	EMail : <support@mwat.de>
+*/
+package hashtags
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+//lint:file-ignore ST1017 - I prefer Yoda conditions
+
+const (
+	testHmStore = "testHmStore.db"
+)
+
+func hmFilename() string {
+	os.Remove(testHmStore)
+
+	return testHmStore
+} // hmFilename()
+
+func prepHashMap() *tHashMap {
+	hm := make(tHashMap, 8)
+	// add already sorted keys
+	hm.add("#hash1", 111).
+		add("#hash2", 222).
+		add("#hash3", 333).
+		add("@mention1", 111).
+		add("@mention2", 222)
+
+	return &hm
+} // prepHashMap()
+
+func Test_tHashMap_add(t *testing.T) {
+	hm1 := prepHashMap()
+	wm1 := prepHashMap().add("@mention2", 222)
+
+	hm2 := prepHashMap().add("#hash4", 444)
+	wm2 := prepHashMap().add("#hash4", 444).add("#hash4", 222)
+
+	type tArgs struct {
+		aName string
+		aID   uint64
+	}
+	tests := []struct {
+		name string
+		hm   *tHashMap
+		args tArgs
+		want tHashMap
+	}{
+		{"1", hm1, tArgs{"@mention2", 222}, *wm1},
+		{"2", hm2, tArgs{"#hash4", 222}, *wm2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.add(tt.args.aName, tt.args.aID); !got.compareTo(tt.want) {
+				t.Errorf("%q: tHashMap.add() =\n%v\n>>>> want: >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_add()
+
+func Test_tHashMap_clear(t *testing.T) {
+	hm1 := prepHashMap()
+	wm1 := tHashMap{}
+
+	tests := []struct {
+		name string
+		hm   *tHashMap
+		want tHashMap
+	}{
+		{"1", hm1, wm1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.clear(); !got.compareTo(tt.want) {
+				t.Errorf("%q: tHashMap.clear() =\n%v\n>>>> want >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_clear()
+
+func Test_tHashMap_compareTo(t *testing.T) {
+	hm1 := prepHashMap()
+	om1 := prepHashMap()
+	om2 := prepHashMap().add("#hash4", 222)
+
+	tests := []struct {
+		name string
+		hm   tHashMap
+		oMap tHashMap
+		want bool
+	}{
+		{"1", *hm1, *om1, true},
+		{"2", *hm1, *om2, false},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.compareTo(tt.oMap); got != tt.want {
+				t.Errorf("%q: tHashMap.compareTo() = %v, want %v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_compareTo()
+
+func Test_tHashMap_count(t *testing.T) {
+	hm1 := prepHashMap()
+	wm1 := 3
+	hm2 := prepHashMap().add("#hash4", 222).sort()
+	wm2 := 4
+	hm3 := prepHashMap().add("@mention3", 222).sort()
+	wm3 := 3
+
+	tests := []struct {
+		name    string
+		hm      tHashMap
+		delim   byte
+		wantInt int
+	}{
+		{"1", *hm1, MarkHash, wm1},
+		{"2", *hm2, MarkHash, wm2},
+		{"3", *hm3, MarkMention, wm3},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotInt := tt.hm.count(tt.delim); gotInt != tt.wantInt {
+				t.Errorf("%q: tHashMap.count() = %v, want %v",
+					tt.name, gotInt, tt.wantInt)
+			}
+		})
+	}
+} // Test_tHashMap_count()
+
+func Test_tHashMap_countedList(t *testing.T) {
+	hm1 := prepHashMap()
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+
+	wc1 := TCountList{
+		{1, "#hash1"},
+		{1, "#hash2"},
+		{1, "#hash3"},
+		{1, "@mention1"},
+		{1, "@mention2"},
+	}
+
+	hm2 := prepHashMap().add("@Alphons", 222)
+	wc2 := TCountList{
+		{1, "#hash1"},
+		{1, "#hash2"},
+		{1, "#hash3"},
+		{1, "@alphons"},
+		{1, "@mention1"},
+		{1, "@mention2"},
+	}
+
+	tests := []struct {
+		name string
+		hm   tHashMap
+		want TCountList
+	}{
+		{"1", *hm1, wc1},
+		{"2", *hm2, wc2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.countedList(); !got.compareTo(tt.want) {
+				t.Errorf("%q: tHashMap.countedList() = \n%v\n>>>> want: >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_countedList()
+
+func Test_tHashMap_idList(t *testing.T) {
+	hm := prepHashMap()
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222).
+
+	// id1 := uint64(111)
+	// wl1 := []string{
+	// 	"#hash1",
+	// 	"#hash2",
+	// 	"#hash3",
+	// 	"@mention1",
+	// 	"@mention2",
+	// }
+
+	id2 := uint64(222)
+	wl2 := []string{
+		"#hash2",
+		"@mention2",
+	}
+	tests := []struct {
+		name string
+		id   uint64
+		want []string
+	}{
+		// {"1", id1, wl1},
+		{"2", id2, wl2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hm.idList(tt.id); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("%q: tHashMap.idList() =\n%v\n>>>> want: >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_idList()
+
+func Test_tHashMap_idxLen(t *testing.T) {
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+	hm := prepHashMap().
+		add("#hash2", 333)
+
+	type tArgs struct {
+		aDelim byte
+		aName  string
+	}
+	tests := []struct {
+		name string
+		args tArgs
+		want int
+	}{
+		{"0", tArgs{}, -1},
+		{"1", tArgs{MarkHash, "#hash2"}, 2},
+		{"2", tArgs{MarkHash, "hash2"}, 2},
+		{"3", tArgs{MarkMention, "@hash2"}, -1},
+		{"4", tArgs{MarkMention, "mention1"}, 1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hm.idxLen(tt.args.aDelim, tt.args.aName); got != tt.want {
+				t.Errorf("%q: tHashMap.idxLen() = %v, want %v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_idxLen()
+
+func Test_tHashMap_keys(t *testing.T) {
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+	hm0 := &tHashMap{}
+	var wl0 []string
+	hm1 := prepHashMap().add("#hash4", 444)
+	wm1 := []string{"#hash1", "#hash2", "#hash3", "#hash4", "@mention1", "@mention2"}
+
+	tests := []struct {
+		name string
+		hm   *tHashMap
+		want []string
+	}{
+		{"0", hm0, wl0},
+		{"1", hm1, wm1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.keys(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("%q: tHashMap.keys() =\n%v\n>>>> want: >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_keys()
+
+func Test_tHashMap_list(t *testing.T) {
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+	hm := prepHashMap().
+		add("#hash3", 33)
+	wl0 := tSourceList{}
+	wl1 := tSourceList{
+		111,
+	}
+	wl2 := tSourceList{
+		33,
+		333,
+	}
+
+	type tArgs struct {
+		aDelim byte
+		aName  string
+	}
+	tests := []struct {
+		name     string
+		args     tArgs
+		wantList tSourceList
+	}{
+		{"0", tArgs{}, wl0},
+		{"1", tArgs{MarkHash, "hash1"}, wl1},
+		{"2", tArgs{MarkHash, "hash3"}, wl2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotList := hm.list(tt.args.aDelim, tt.args.aName)
+			if !tt.wantList.compareTo(gotList) {
+				t.Errorf("%q: tHashMap.list() =\n%v\n>>>> want: >>>>\n%v",
+					tt.name, gotList, tt.wantList)
+			}
+		})
+	}
+} // Test_tHashMap_list()
+
+func Test_tHashMap_remove(t *testing.T) {
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+	hm := prepHashMap().
+		add("#nameX", 999).
+		add("#hash3", 333)
+	// wm0 := hm
+
+	wm1 := prepHashMap().
+		add("#nameX", 999).
+		add("#hash3", 333)
+
+	wm2 := prepHashMap().
+		add("#hash3", 333)
+
+	type tArgs struct {
+		aDelim byte
+		aName  string
+		aID    uint64
+	}
+	tests := []struct {
+		name string
+		args tArgs
+		want tHashMap
+	}{
+		// {"0", tArgs{}, *wm0},
+		{"1", tArgs{MarkHash, "#nameX", 111}, *wm1},
+		{"2", tArgs{MarkHash, "#nameX", 999}, *wm2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := hm.remove(tt.args.aDelim, tt.args.aName, tt.args.aID)
+			if !got.compareTo(tt.want) {
+				t.Errorf("%q: tHashMap.remove() =\n%v\n>>>> want >>>>\n%v",
+					tt.name, got, tt.want.String())
+			}
+		})
+	}
+} // Test_tHashMap_remove()
+
+func Test_tHashMap_removeID(t *testing.T) {
+
+	tests := []struct {
+		name string
+		hm   *tHashMap
+		id   uint64
+		want *tHashMap
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.removeID(tt.id); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("tHashMap.removeID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_removeID()
+
+func Test_tHashMap_renameID(t *testing.T) {
+	//	add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+	id1, id2, id3 := uint64(11), uint64(22), uint64(33)
+
+	// hm1 := prepHashMap().
+	// 	add("#hash1", id1).
+	// 	add("#hash1", id3).
+	// 	add("@mention1", id3).
+	// 	sort()
+	// wm1 := prepHashMap().
+	// 	add("#hash1", id2).
+	// 	add("#hash1", id3).
+	// 	add("@mention1", id3).
+	// 	sort()
+	if 0 == id1 {
+		return
+	}
+
+	hm2 := prepHashMap().
+		add("#hash3", id2).
+		add("#hash4", id2).
+		sort()
+	wm2 := prepHashMap().
+		add("#hash3", id3).
+		add("#hash4", id3).
+		sort()
+
+	type tArgs struct {
+		aOldID, aNewID uint64
+	}
+	tests := []struct {
+		name string
+		hm   *tHashMap
+		args tArgs
+		want *tHashMap
+	}{
+		// {"0", hm1, tArgs{}, hm1}, // no change
+		// {"1", hm1, tArgs{id1, id2}, wm1},
+		{"2", hm2, tArgs{id2, id3}, wm2},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.renameID(tt.args.aOldID, tt.args.aNewID); !got.compareTo(*tt.want) {
+				t.Errorf("%q: tHashMap.renameID() =\n%v\n>>>> want: >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_renameID
+
+func Test_tHashMap_sort(t *testing.T) {
+	hm1 := &tHashMap{
+		"#hash1": &tSourceList{
+			uint64(111),
+		},
+		"@mention1": &tSourceList{
+			uint64(111),
+		},
+		"#hash2": &tSourceList{
+			uint64(222),
+		},
+		"@mention2": &tSourceList{
+			uint64(333),
+			uint64(222),
+		},
+		"#hash3": &tSourceList{
+			uint64(333),
+		},
+	}
+	wm1 := tHashMap{
+		"#hash1": &tSourceList{
+			uint64(111),
+		},
+		"#hash2": &tSourceList{
+			uint64(222),
+		},
+		"#hash3": &tSourceList{
+			uint64(333),
+		},
+		"@mention1": &tSourceList{
+			uint64(111),
+		},
+		"@mention2": &tSourceList{
+			uint64(222),
+			uint64(333),
+		},
+	}
+
+	tests := []struct {
+		name string
+		hm   *tHashMap
+		want tHashMap
+	}{
+		{"1", hm1, wm1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.hm.sort(); !got.compareTo(tt.want) {
+				t.Errorf("%q: tHashMap.sort() = \n%v\n>>>> want: >>>>\n%v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_sort()
+
+func Test_tHashMap_store(t *testing.T) {
+	// hm.add("#hash1", 111).
+	// 	add("#hash2", 222).
+	// 	add("#hash3", 333).
+	// 	add("@mention1", 111).
+	// 	add("@mention2", 222)
+	hm1 := prepHashMap().
+		add("@alphons", 1)
+	fn := hmFilename()
+
+	tests := []struct {
+		name      string
+		hm        tHashMap
+		useBinary bool
+		want      int
+		wantErr   bool
+	}{
+		{"1", *hm1, false, 84, false},
+		{"2", *hm1, true, 109, false},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			UseBinaryStorage = tt.useBinary
+			got, err := tt.hm.store(fn + tt.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("%q: tHashMap.store() error = %v, wantErr %v",
+					tt.name, err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("%q: tHashMap.store() = %v, want %v",
+					tt.name, got, tt.want)
+			}
+		})
+	}
+} // Test_tHashMap_store()
+
+func Test_tHashMap_String(t *testing.T) {
+	//
+	// NOTE: Since the order of the hashtags/mentions is NOT guaranteed
+	// here the order of the the hashtags/mentions im the returned String
+	// isn't guaranteed to be ordered either.
+	//
+	sl0 := &tHashMap{}
+	ws0 := ""
+	sl1 := prepHashMap()
+	ws1 := "[#hash1]\n111\n[#hash2]\n222\n[#hash3]\n333\n[@mention1]\n111\n[@mention2]\n222\n"
+
+	tests := []struct {
+		name     string
+		hm       *tHashMap
+		wantRStr string
+	}{
+		{"0", sl0, ws0},
+		{"1", sl1, ws1},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRStr := tt.hm.String(); gotRStr != tt.wantRStr {
+				t.Errorf("%q: tHashMap.String() = \n%v\n>>>> want: >>>>\n%v",
+					tt.name, gotRStr, tt.wantRStr)
+			}
+		})
+	}
+} // Test_tHashMap_String()
+
+func Test_tHashMap_walk(t *testing.T) {
+	type args struct {
+		aFunc TWalkFunc
+	}
+	tests := []struct {
+		name      string
+		hm        *tHashMap
+		args      args
+		wantRBool bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRBool := tt.hm.walk(tt.args.aFunc); gotRBool != tt.wantRBool {
+				t.Errorf("tHashMap.walk() = %v, want %v", gotRBool, tt.wantRBool)
+			}
+		})
+	}
+}
